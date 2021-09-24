39e68c9b785dae3d8222054a27d20c96d3264f2f
robinNcode <msmrobin518@gmail.com>
2021-09-24 20:56:33 +0600

    implicit & explicit casting done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,50 @@ namespace Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\t\t\t\tType Casting Assignment");
+            Console.WriteLine("\t\t\t---------------------------------------------");
+
+            //Implicit casting class starts here
+            MyImplicitCast imp = new MyImplicitCast();
+            Console.WriteLine("\t<<< Implicit Casting >>>");
+            Console.Write("Enter Age: ");
+            imp.age = Convert.ToInt32(Console.ReadLine());
+
+            imp.ImpCast();
+
+            //Explicit casting class starts here
+            MyExplicitCast Exmp = new MyExplicitCast();
+            Console.WriteLine("\t<<< Explicit Casting >>>");
+            Console.Write("Enter Age: ");
+            Exmp.age = Convert.ToDouble(Console.ReadLine());
+
+            Exmp.ExmpCast();
+
         }
     }
+
+    class MyImplicitCast
+    {
+        public int age;
+        public double newAge;
+
+        public void ImpCast(){
+            Console.WriteLine("Int value : " + this.age);
+            this.newAge = this.age; // Automatic casting: int to double
+            Console.WriteLine("Double value : " + this.newAge);
+        }
+    }
+
+    class MyExplicitCast
+    {
+        public double age;
+        public int newAge;
+
+        public void ExmpCast(){
+            Console.WriteLine("Double value : " + this.age);
+            this.newAge = (int)this.age; // Automatic casting: int to double
+            Console.WriteLine("Int value : " + this.newAge);
+        }
+    }
+
 }
